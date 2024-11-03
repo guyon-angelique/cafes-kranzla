1efc7a3a9e450691abae0b56c7c4d3a0b2e757e4
guyon-angelique <112342626+guyon-angelique@users.noreply.github.com>
2024-11-03 12:46:02 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('v1').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/offline.html', // Page à afficher quand hors connexion
+          '/styles.css',
+          '/app.js',
+          '/api.js',
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches.match(event.request).then((response) => {
+          return response || caches.match('/offline.html'); // Renvoie la page hors ligne si la requête échoue
+        });
+      })
+    );
+  });
+  
