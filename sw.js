4fae17f8394006ef40c8454834dfe0c129010fd8
guyon-angelique <112342626+guyon-angelique@users.noreply.github.com>
2024-11-03 19:25:37 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
 self.addEventListener('install', (event) => {
     event.waitUntil(
-      caches.open('v4').then((cache) => {
+      caches.open('v5').then((cache) => {
         return cache.addAll([
           'offline.html', // Page à afficher quand hors connexion
           'app.js',
           'api.js',
           'images/cafe.png',
+          'pulltorefresh.js',
           'site.manifest'
         ]);
       })
